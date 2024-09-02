e149d236d6580850f2867b18f60cd2b509d02eb7
noefforted <ahmadopa73gmail.com>
2024-09-02 09:41:12 +0700

    nambah warna

--- installer.sh
+++ installer.sh
@@ -1,27 +1,31 @@
-echo '>>> Instalation begin... <<<'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+echo "${CYAN}>>> Installation begin... <<<${NC}"
 sleep 2
-echo '>>> Installing Mosquitto <<<'
+
+echo "${CYAN}>>> Installing Mosquitto <<<${NC}"
 sudo apt install -y mosquitto
 
-echo '>>> Making Virtual Environtment <<<'
+echo "${CYAN}>>> Making Virtual Environment <<<${NC}"
 python -m venv venv_LED_DHT11
-source venv_LED_DHT11/activate
+source venv_LED_DHT11/bin/activate
 
-echo '>>> Installing Required libraries <<<'
+echo "${CYAN}>>> Installing Required libraries <<<${NC}"
 pip install -r requirements.txt
 
-echo '>>> Raspberry configurations <<<'
+echo "${CYAN}>>> Raspberry configurations <<<${NC}"
 sudo raspi-config nonint do_i2c 0
 
-echo '>>> Setup Firewall <<<'
+echo "${CYAN}>>> Setup Firewall <<<${NC}"
 sudo apt install -y ufw=0.36-7.1
 sudo ufw allow ssh
 
-echo '>>> Setup reverse SSH tunnel <<<'
+echo "${CYAN}>>> Setup reverse SSH tunnel <<<${NC}"
 ssh -f -N -R 5050:localhost:1883 ubuntu@13.213.41.188 -i AWS-Widya.pem 
 
-echo '>>> Run main.py <<<'
+echo "${CYAN}>>> Run main.py <<<${NC}"
 python3 main.py
 
-echo '>>> Installation Finished <<<'
-echo 'Now you can install Node-RED on client device and import flows.json'
+echo "${CYAN}>>> Installation Finished <<<${NC}"
+echo "${CYAN}Now you can install Node-RED on client device and import flows.json${NC}"
